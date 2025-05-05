d63f060d31dc2863a0cec8682440f9a0b19038a6
“Ivona” <ivona.rieger@gmail.com>
2025-05-05 19:54:27 +0200

    add content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,10 @@ import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
+
+
+
 
 function App() {
   return (
